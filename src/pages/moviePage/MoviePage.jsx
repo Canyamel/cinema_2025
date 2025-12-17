2737429772e450e234be8967386ac60979429518
Canyamel <66073415+Canyamel@users.noreply.github.com>
2025-12-17 23:06:21 +0300

    Add files via upload

--- src/pages/moviePage/MoviePage.jsx
+++ src/pages/moviePage/MoviePage.jsx
@@ -1,96 +1,127 @@
 import { Descriptions, Flex, Typography, Tag } from "antd";
+import { getMovie } from "../../utils/requests";
 import "./MoviePage.css";
 
 const MoviePage = () => {
-  const movie = {
-    id: 1,
-    title: {
-      ru: "Дюна: Часть вторая",
-      en: "Dune: Part Two",
-    },
-    rating: 8.2,
-    details: {
-      year: 2024,
-      director: "Дени Вильнёв",
-      screenwriter: "Дени Вильнёв",
-      genres: ["драма", "боевик", "фантастика"],
-      country: "США",
-      description:
-        "Герцог Пол Атрейдес присоединяется к фрименам, чтобы стать Муад'Дибом, одновременно пытаясь остановить наступление войны.",
-    },
-    poster:
-      "https://avatars.mds.yandex.net/get-kinopoisk-image/4486454/34ecdec6-c76f-4cd7-818c-905803184ed8/orig",
-    actors: [
-      "Тимоти Шаламе",
-      "Зендая",
-      "Ребекка Фергюсон",
-      "Хавьер Бардем",
-      "Остин Батлер",
-    ],
-  };
-  const items = [
-    { label: "год", children: movie.details.year },
-    {
-      label: "режиссер",
-      children: movie.details.director,
-    },
-    {
-      label: "сценарист",
-      children: movie.details.screenwriter,
-    },
-    {
-      label: "жанр",
-      children: (
-        <>
-          {movie.details.genres.map((genre) => (
-            <Tag color="#FF7A85" key={genre}>
-              {genre}
-            </Tag>
-          ))}
-        </>
-      ),
-    },
-    {
-      label: "страна",
-      children: movie.details.country,
-    },
-    {
-      label: "описание",
-      children: movie.details.description,
-      span: 3,
-    },
-  ];
+    const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  return (
-    <div id="movie-page">
-      <div className="content-movie-container">
-        <Flex gap={30} align="center">
-          <Typography.Title className="movie-title" level={4}>
-            {movie.title.ru}
-          </Typography.Title>
-          <Typography.Title className="movie-title light" level={4}>
-            {movie.title.en}
-          </Typography.Title>
-        </Flex>
-      </div>
-      <div className="content-movie-container">
-        <Flex gap={90}>
-          <img className="movie-img" src={movie.poster}></img>
-          <div className="movie-info-container">
-            <Flex></Flex>
-            <Descriptions
-              column={1}
-              colon={false}
-              title={"О фильме"}
-              bordered
-              items={items}
-            ></Descriptions>
-          </div>
-        </Flex>
-      </div>
-      <div className="content-movie-container"></div>
-    </div>
-  );
+    useEffect(() => {
+        if (id) {
+            getMovie(id)
+            .then((res) => {
+                setMovie(res.data.data);
+            })
+            .catch((err) => {
+				console.error('Ошибка загрузки фильма:', err)
+				setError('Не удалось загрузить информацию о фильме')
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+        }
+    }, [id]);
+
+    if (loading) {
+        return (
+            <div id="movie-page" className="content-movie-container">
+                <Flex justify="center" align="center" style={{ height: "400px" }}>
+                    <Typography.Title level={3}>
+                        Загрузка фильма...
+                    </Typography.Title>
+                </Flex>
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div id="movie-page" className="content-movie-container">
+                <Typography.Title level={3} style={{ color: "#ff4d4f" }}>
+                    {error}
+                </Typography.Title>
+            </div>
+        );
+    }
+
+    if (!movie) {
+        return (
+            <div id="movie-page" className="content-movie-container">
+                <Typography.Title level={3}>
+                    Фильм не найден
+                </Typography.Title>
+            </div>
+        );
+    }
+
+    const items = [
+        {
+            label: "год",
+            children: movie.details.year
+        },
+        {
+            label: "режиссер",
+            children: movie.details.director
+        },
+        {
+            label: "сценарист",
+            children: movie.details.screenwriter
+        },
+        {
+            label: "жанр",
+            children: (
+                <>
+                    {movie.details.genres.map((genre) => (
+                        <Tag color="#FF7A85" key={genre}>
+                            {genre}
+                        </Tag>
+                    ))}
+                </>
+            )
+        },
+        {
+            label: "страна",
+            children: movie.details.country
+        },
+        {
+            label: "описание",
+            children: movie.details.description,
+            span: 3
+        }
+    ];
+
+    return (
+        <div id="movie-page">
+            <div className="content-movie-container">
+                <Flex gap={30} align="center">
+                    <Typography.Title className="movie-title" level={4}>
+                        {movie.title.ru}
+                    </Typography.Title>
+                    <Typography.Title className="movie-title light" level={4}>
+                        {movie.title.en}
+                    </Typography.Title>
+                </Flex>
+            </div>
+            <div className="content-movie-container">
+                <Flex gap={90}>
+                    <img className="movie-img" src={movie.poster}></img>
+                    <div className="movie-info-container">
+                        <Flex></Flex>
+                            <Descriptions
+                                column={1}
+                                colon={false}
+                                title={"О фильме"}
+                                bordered
+                                items={items}
+                            ></Descriptions>
+                    </div>
+                </Flex>
+            </div>
+            <div className="content-movie-container"></div>
+        </div>
+    );
 };
 
 export default MoviePage;
